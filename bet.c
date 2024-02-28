2e2120f574c36884f4e7749eb0318d63cbae76a5
root <kelvinimere@gmail.com>
2024-02-28 12:22:19 -0800

    simple_shell

---
+++ bet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program to print alphabet
+ * Return: void
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		printf(k);
+	}
+	printf('\n');
+	return (0);
+}
